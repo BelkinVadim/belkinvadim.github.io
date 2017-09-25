0210dd1be4c96e7f723ec48669510808674926da
Вадим <belkinvadim@outlook.com>
2017-09-25 23:26:28 +0400

    ``

--- apps/notes/js/loader.js
+++ apps/notes/js/loader.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('serviceworker.js')
+    navigator.serviceWorker.register('../serviceworker.js')
         .then(function(reg) {
             // регистрация сработала
             console.log('Registration succeeded. Scope is ' + reg.scope);
